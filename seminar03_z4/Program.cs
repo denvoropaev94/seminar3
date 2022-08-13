ad399086a948c892aa9f5e117a2bdb8cb1617b22
«Denis» <denvoropaev7@gmail.com>
2022-08-13 10:59:46 +0300

    Задача принимает число и показывает квадраты от 1 до этого числа

---
+++ seminar03_z4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string PrintsТumbersSquares(int num)
+{
+string result = "";
+    for (int i = 1; i <= num; i++)
+    {
+        result += Math.Pow(i,2);
+    }
+    return result;
+}
+string result = PrintsТumbersSquares(num);
+Console.WriteLine(result);

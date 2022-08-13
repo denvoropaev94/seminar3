78bc13e6bd1fdcc0fcf035809ea4e572db96838b
«Denis» <denvoropaev7@gmail.com>
2022-08-13 14:52:24 +0300

    Задача палиндром, работает инверсия пока что

---
+++ seminar03_dz19/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string str = Console.ReadLine();
+string newstr = "";
+
+string InversionString (string str)
+{
+
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+        newstr += str[i];
+    }
+    return newstr;
+}
+bool IsPolindrom(string str){
+    if(str==newstr)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool result = IsPolindrom(str);
+Console.WriteLine(result);
+
+
+

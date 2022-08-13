7d559bd0650537926c173136ce0b4874d2009279
«Denis» <denvoropaev7@gmail.com>
2022-08-13 10:55:55 +0300

    Задача показывает координаты по заданной четверти

---
+++ seminar03_z2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string ShowCoordinateRange(int number){
+
+    if (number == 1)
+    {
+        return "x > 0 && y > 0";
+    }
+     if (number == 2)
+    {
+        return "x < 0 && y > 0";
+    }
+     if (number == 3)
+    {
+        return "x < 0 && y < 0";
+    }
+     if (number == 4)
+    {
+        return "x > 0 && y < 0";
+    }
+    throw new Exception("Такой четверти не существует!");
+}
+
+string input = ShowCoordinateRange(number);
+Console.WriteLine(input);

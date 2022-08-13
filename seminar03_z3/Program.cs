44a4b226206197887892b99d03f80084957d45e8
«Denis» <denvoropaev7@gmail.com>
2022-08-13 10:58:01 +0300

    Задача принимает координаты и находит расстояние между ними в 2D пространстве

---
+++ seminar03_z3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите точку х1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку y2"); 
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double FindDistanse(int x1, int y1, int x2, int y2)
+{
+    double XY = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+    return XY;
+}
+double result = FindDistanse(x1, y1, x2, y2);
+Console.WriteLine(result);

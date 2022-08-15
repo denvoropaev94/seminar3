4668226873833eeec0210291b6b957f1b2e87dd4
«Denis» <denvoropaev7@gmail.com>
2022-08-15 09:30:06 +0300

    Задача на входе принимает число и выдает таблицу кубов

---
+++ seminar03_dz23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] TableOfCubes(int s)
+{
+    int[] array = new int[s];
+    for (int i = 1; i <= N; i++)
+    {
+        array[i - 1] = i * i * i;
+    }
+    return array;
+}
+int[] result = TableOfCubes(N);
+for (int i = 0; i < result.Length; i++){
+    Console.WriteLine(result[i]);
+}

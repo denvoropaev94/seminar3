5a52590557805e757a249a3ed6954f50ea4e5ad4
«Denis» <denvoropaev7@gmail.com>
2022-08-17 09:17:41 +0300

    Задача,принимает на вход число и выдает произведение от 1 до этого числа

---
+++ seminar04_z2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string N = (Console.ReadLine());
+
+
+bool isNum = int.TryParse(N, out int result);
+if(isNum == false){
+    Console.WriteLine("Введите число, а не символы какие-то...");
+    return;
+}
+
+int GetProizvedenie(int number){
+    int result = 1;
+    for (int i =1;i<=number;i++){
+        result = result * i;
+    }
+    return result;
+}
+int res = GetProizvedenie(result);
+Console.WriteLine(res);

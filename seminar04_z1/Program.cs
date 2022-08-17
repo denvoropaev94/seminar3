4e071fb44b90a3474ddbfa2ff553c072a9e906b8
«Denis» <denvoropaev7@gmail.com>
2022-08-17 09:15:31 +0300

    Задача,принимает на вход число и выдает количество цифр

---
+++ seminar04_z1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int CheckNumbers(int number){
+
+   int count = 0;
+   while (n>0){
+    n = n/10;
+    count++;
+   }
+   return count;
+}
+int display = CheckNumbers(n);
+Console.WriteLine(display);

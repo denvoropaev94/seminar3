c0205fa8d4d80ea71d42a3b7cfdf826d53ae589c
«Denis» <denvoropaev7@gmail.com>
2022-08-17 09:18:53 +0300

    Задача, которая выводит массив случайными числами

---
+++ seminar04_z30/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] FillArray (int[] collection)//Метод наполнения массива
+{
+    for(int i=0; i< collection.Length;i++){
+        collection[i] = new Random().Next(0,2);
+    }
+    return collection;
+}
+
+void PrintArray(int[] col)//Метод, который печает массив
+{
+    for(int i = 0;i<col.Length;i++)
+    {
+        Console.WriteLine($"{col[i]} ");
+    }
+}
+int[] array = new int[8];
+
+int[] n  = FillArray(array);
+PrintArray(n);

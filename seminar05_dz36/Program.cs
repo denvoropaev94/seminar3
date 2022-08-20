71b4c1483a0fb133fefa273bc8b590a10220895f
«Denis» <denvoropaev7@gmail.com>
2022-08-20 14:07:07 +0300

    Программа заполняет массив случайными числами и выводит сумму элементов, стоящих на нечетных позициях

---
+++ seminar05_dz36/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int[] array = new int[5];
+
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(1,100);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int FindSumOddElements(int[]arr){
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int[] result = FillArray(array);
+PrintArray(result);
+System.Console.WriteLine();
+int sumResult = FindSumOddElements(result);
+System.Console.WriteLine(sumResult);

0385e8cde00ce2e64ab1ec3e2be9beef429c6881
«Denis» <denvoropaev7@gmail.com>
2022-08-20 10:56:13 +0300

    Задача, которая заменяет элементы массива, положительные на отрицательные и наооборт

---
+++ seminar05_z32/Program.cs
@@ -0,0 +1,11 @@
+﻿int[]array = {4,8,-8,-2};
+int[] ChangeNumber(int[]arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+int[] result = ChangeNumber(array);
+string resultNum = string.Join("|",result);
+Console.WriteLine(resultNum);

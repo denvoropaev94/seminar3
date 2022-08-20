10f804d716d85fe40baddff7cc9d8a02e48f1bd7
«Denis» <denvoropaev7@gmail.com>
2022-08-20 10:40:22 +0300

    Задача, которая на вход принимает число и опредеяет есть ли это число в заданном массиве

---
+++ seminar05_z33/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int[] arr = new int[] { 6, 7, 19, 345, 3 }; 
+ 
+bool FindNumber(int N, int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i] == N) 
+        { 
+            return true; 
+        } 
+    } 
+    return false; 
+} 
+
+bool res = FindNumber(number, arr);
+if(res==true){
+    Console.WriteLine("Данное число есть в массиве");
+} 
+else{
+    Console.WriteLine("Данного числа нет в массиве!");
+}

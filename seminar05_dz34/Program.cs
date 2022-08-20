dd92eef14fc464cb1e4f36c53e605cc6faede42f
«Denis» <denvoropaev7@gmail.com>
2022-08-20 13:40:15 +0300

    Программа заполняет массив случайными 3-ными числами и показывает количество четных чисел

---
+++ seminar05_dz34/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+int[] array = new int[4];
+
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100,1000);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int ShowCountPozitiveElements(int[] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]%2==0){
+        count++;}
+    }
+    return count;
+}
+int[] result = FillArray(array);
+PrintArray(result);
+System.Console.WriteLine();
+int countResut = ShowCountPozitiveElements(result);
+System.Console.WriteLine(countResut);

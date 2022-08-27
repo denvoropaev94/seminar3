b7c5db5ddda848f53a29333fcaebc53c05eed57c
«Denis» <denvoropaev7@gmail.com>
2022-08-27 11:21:51 +0300

    Задаем двумерный массив размером m на n случайными вещественными числами

---
+++ seminar07_dz47/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите количество столбцов");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (isNumberM == false || isNumberN == false)
+{
+    System.Console.WriteLine("Введены не правильные данные");
+    return;
+}
+
+double[,] FillArray2D(int m, int n)
+{
+    double[,] array = new double[m,n];
+    Random random = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = Convert.ToDouble(random.Next(-100, 100)/10.0);
+        }
+    }
+    return  array;
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] result = FillArray2D(m,n);
+PrintArray2D(result);

55007e5b17c4399e8e75c7e666c0624af1280b93
«Denis» <denvoropaev7@gmail.com>
2022-09-03 11:51:54 +0300

    Программа, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии

---
+++ seminar09_dz66/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+System.Console.WriteLine("Введие первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введие второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int PrintNumbers(int m,int n){
+    // System.Console.Write($" {index}");
+    if(m==n) return m;
+    if(m<n){
+
+        return m + PrintNumbers(m+1, n);
+    }
+    throw new Exception();
+
+}
+int result = PrintNumbers(m, n);
+System.Console.WriteLine(result);

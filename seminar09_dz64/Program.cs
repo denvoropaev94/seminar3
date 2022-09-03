122ae92d182c398a1a7593f92ffbc2bdc84ec9fb
«Denis» <denvoropaev7@gmail.com>
2022-09-03 11:46:51 +0300

    Программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии

---
+++ seminar09_dz64/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+System.Console.WriteLine("Введие первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введие второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+void PrintNumbers(int index,int num){
+    System.Console.Write($" {index}");
+    if(index<num){
+
+        PrintNumbers(index + 1,num);
+    }
+
+}
+PrintNumbers(m,n);
